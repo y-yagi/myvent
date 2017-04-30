8a5f1b3bd87fc7699bfc7aebff3cc3bee05cf91d
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2017-04-30 12:55:41 +0900

    set time zone and default locale

--- config/application.rb
+++ config/application.rb
@@ -21,8 +21,12 @@ module Myvent
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
+
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
   end
 end
