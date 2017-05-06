928315b67ca352afa60d639fb8bd3247651684a7
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2017-05-06 14:28:50 +0900

    add test for event model

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  sub_test_case('validation') do
+    test 'should have the necessary required validators' do
+      event = Event.new(user: users(:google))
+      assert_not event.valid?
+      assert_equal [:name], event.errors.keys
+    end
+  end
+
+  test 'build return Event instance' do
+    params = ActionController::Parameters.new(name: 'ダミー')
+    params.permit!
+    event = Event.build(params, users(:google))
+    assert_instance_of Event, event
+    assert_equal users(:google).id, event.user_id
+    assert_equal 'ダミー', event.name
+  end
 end
