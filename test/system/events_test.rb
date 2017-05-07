0d6139d66bd916c77e12f1d0de6f77c88fd792e7
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2017-05-07 13:32:48 +0900

    add test for create event

--- test/system/events_test.rb
+++ test/system/events_test.rb
@@ -7,4 +7,26 @@ class EventsTest < ApplicationSystemTestCase
     visit events_url
     assert_match 'イベントリスト', page.text
   end
+
+  test "create new event" do
+    visit events_url
+    click_on '登録'
+
+    fill_in('event[name]', with: 'ブリューゲル「バベルの塔」展')
+    fill_in('event[start_at]', with: '2017-04-18')
+    fill_in('event[end_at]', with: '2017-07-02')
+    fill_in('event[url]', with: 'http://babel2017.jp/')
+    fill_in('event[place_name]', with: '東京都美術館')
+    fill_in('event[memo]', with: '前売り券はもってない')
+    click_on '登録'
+
+    assert_match 'ブリューゲル「バベルの塔」展', page.text
+    event = Event.last
+    assert_equal 'ブリューゲル「バベルの塔」展', event.name
+    assert_equal '2017-04-18', event.start_at.to_s
+    assert_equal '2017-07-02', event.end_at.to_s
+    assert_equal 'http://babel2017.jp/', event.url
+    assert_equal '東京都美術館', event.place_name
+    assert_equal '前売り券はもってない', event.memo
+  end
 end
